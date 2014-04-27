a26f1f2dc3200fe1b878cf01988fc3a7ff00e7e8
Shota Fukumori (sora_h) <her@sorah.jp>
2014-04-28 02:22:13 +0900

    Initial nicolive support
    - Supporting timeshifts only

---
+++ lib/niconico/live.rb
@@ -0,0 +1,172 @@
+# coding: utf-8
+require 'niconico/live/api'
+
+class Niconico
+  def live(live_id)
+    Live.new(self, live_id)
+  end
+
+  class Live
+    class ReservationOutdated < Exception; end
+    class ReservationNotAccepted < Exception; end
+    class TicketRetrievingFailed < Exception; end
+
+    def initialize(parent, live_id)
+      @parent = parent
+      @agent = parent.agent
+      @id = @live_id = live_id
+      @client = Niconico::Live::API.new(@agent)
+
+      get()
+    end
+
+    attr_reader :id, :live, :ticket
+
+    def fetched?
+      !!@fetched
+    end
+
+    def get(force=false)
+      return self if @fetched && !force
+      @live = @client.get(@live_id)
+      @fetched = true
+      self
+    end
+
+    def seat(force=false)
+      return @seat if @seat && !force
+      raise ReservationNotAccepted if reserved? && !reservation_accepted?
+
+      @seat = @client.get_player_status(self.id)
+
+      raise TicketRetrievingFailed, @seat[:error] if @seat[:error]
+
+      @seat
+    end
+
+    def accept_reservation
+      return self if reservation_accepted?
+      raise ReservationOutdated if reservation_outdated?
+
+      @client.accept_watching_reservation(self.id)
+
+      self
+    end
+
+    def title
+      get.live[:title]
+    end
+
+    def description
+      get.live[:description]
+    end
+
+    def opens_at
+      get.live[:opens_at]
+    end
+
+    def starts_at
+      get.live[:starts_at]
+    end
+
+    def status
+      get.live[:status]
+    end
+
+    def scheduled?
+      status == :scheduled
+    end
+
+    def on_air?
+      status == :on_air
+    end
+
+    def closed?
+      status == :closed
+    end
+
+    def reserved?
+      get.live.key? :reservation
+    end
+
+    def reservation_unaccepted?
+      reserved? && live[:reservation][:status] == :reserved
+    end
+
+    def reservation_accepted?
+      reserved? && live[:reservation][:status] == :accepted
+    end
+
+    def reservation_outdated?
+      reserved? && live[:reservation][:status] == :outdated
+    end
+
+    def reservation_available?
+      reservation_unaccepted? || reservation_accepted?
+    end
+
+    def reservation_expires_at
+      reserved? ? live[:reservation][:expires_at] : nil
+    end
+
+    def channel
+      get.live[:channel]
+    end
+
+    def premium?
+      !!seat[:premium?]
+    end
+
+    def rtmp_url
+      seat[:rtmp][:url]
+    end
+
+    def ticket
+      seat[:rtmp][:ticket]
+    end
+
+    def quesheet
+      seat[:quesheet]
+    end
+
+    def rtmpdump_commands(file_base)
+      file_base = File.expand_path(file_base)
+
+      publishes = quesheet.select{ |_| /^\/publish / =~ _[:body] }.map do |publish|
+        publish[:body].split(/ /).tap(&:shift)
+      end
+
+      plays = quesheet.select{ |_| /^\/play / =~ _[:body] }
+ 
+      plays.map.with_index do |play, i|
+        cases = play[:body].split(/ /)[1].split(/,/)
+        publish_id = nil
+
+        publish_id   = cases.find { |_| _.start_with?('premium:') } if premium?
+        publish_id ||= cases.find { |_| _.start_with?('default:') }
+
+        publish_id = publish_id.split(/:/).last
+
+        contents = publishes.select{ |_| _[0] == publish_id }
+
+        contents.map.with_index do |content, j|
+          content = content[1]
+          rtmp = "#{self.rtmp_url}/mp4:#{content}"
+
+          seq = 0
+          begin
+            file = "#{file_base}.#{i}.#{j}.#{seq}.flv"
+            seq += 1
+          end while File.exist?(file)
+
+          ['rtmpdump',
+           '-V',
+           '-o', file,
+           '-r', rtmp,
+           '-C', "S:#{ticket}",
+          ]
+        end
+      end.flatten(1)
+    end
+  end
+end
---
+++ lib/niconico/live/api.rb
@@ -0,0 +1,180 @@
+# coding: utf-8
+require 'time'
+
+class Niconico
+  class Live
+    class API
+      URL_GETPLAYERSTATUS = 'http://ow.live.nicovideo.jp/api/getplayerstatus'.freeze
+      URL_WATCHINGRESERVATION_LIST = 'http://live.nicovideo.jp/api/watchingreservation?mode=list'
+
+      def initialize(agent)
+        @agent = agent
+      end
+
+      attr_reader :agent
+
+      def get(id)
+        id = normalize_id(id)
+
+        page = agent.get("http://live.nicovideo.jp/gate/#{id}")
+
+        comment_area = page.at("#comment_area#{id}").inner_text
+
+        result = {
+          title: page.at('h2 span').inner_text,
+          id: id,
+          description: page.at('.stream_description .text_area').inner_html,
+        }
+
+        kaijo = page.search('.kaijo strong').map(&:inner_text)
+        result[:opens_at] = Time.parse("#{kaijo[0]} #{kaijo[1]} +0900")
+        result[:starts_at] = Time.parse("#{kaijo[0]} #{kaijo[2]} +0900")
+
+        result[:status] = :scheduled if comment_area.include?('開場まで、あと')
+        result[:status] = :on_air if comment_area.include?('現在放送中')
+        close_message = comment_area.match(/この番組は(.+?)に終了いたしました。/)
+        if close_message
+          result[:status] = :closed
+          result[:closed_at] = Time.parse("#{close_message[1]} +0900")
+        end
+
+        reservation_valid_until_message = comment_area.match(/使用期限: (.+?)まで/)
+        if reservation_valid_until_message 
+          result[:reservation] = {}
+          result[:reservation][:expires_at] = Time.parse("#{reservation_valid_until_message[1]} +0900")
+
+          if comment_area.include?('視聴権を使用し、タイムシフト視聴を行いますか？')
+            result[:reservation][:status] = :reserved
+            result[:reservation][:available] = true
+          elsif comment_area.include?('本番組は、タイムシフト視聴を行う事が可能です。')
+            result[:reservation][:status] = :accepted
+            result[:reservation][:available] = true
+          elsif comment_area.include?('タイムシフト視聴をこれ以上行う事は出来ません。') || comment_area.include?('視聴権の利用期限が過ぎています。')
+            result[:reservation][:status] = :outdated
+          end
+        end
+
+        channel = page.at('div.chan')
+        if channel
+          result[:channel] = {
+            name: channel.at('.shosai a').inner_text,
+            id: channel.at('.shosai a')['href'].split('/').last,
+            link: channel.at('.shosai a')['href'],
+          }
+        end
+
+        result
+      end
+
+      def heartbeat
+        raise NotImplementedError
+      end
+
+      def get_player_status(id)
+        id = normalize_id(id)
+        page = agent.get("http://ow.live.nicovideo.jp/api/getplayerstatus?locale=GLOBAL&lang=ja%2Djp&v=#{id}&seat%5Flocale=JP")
+        status = page.at('getplayerstatus')
+
+        if status['status'] == 'fail'
+          error = page.at('error code').inner_text
+
+          case error
+          when 'notlogin'
+            return {error: :not_logged_in}
+          when 'comingsoon'
+            return {error: :not_yet_started}
+          when 'closed'
+            return {error: :closed}
+          when 'require_accept_print_timeshift_ticket'
+            return {error: :reservation_not_accepted}
+          when 'timeshift_ticket_expire'
+            return {error: :reservation_expired}
+          when 'noauth'
+            return {error: :archive_closed}
+          when 'notfound'
+            return {error: :not_found}
+          else
+            return {error: error}
+          end
+        end
+
+        result = {}
+
+        # Strings
+        %w(id title description provider_type owner_name
+           bourbon_url full_video kickout_video).each do |key|
+          item = status.at(key)
+          result[key.to_sym] = item.inner_text if item
+        end
+
+        # Integers
+        %w(watch_count comment_count owner_id watch_count comment_count).each do |key|
+          item = status.at(key)
+          result[key.to_sym] = item.inner_text.to_i if item
+        end
+
+        # Flags
+        %w(is_premium is_reserved is_owner international is_rerun_stream is_archiveplayserver
+           archive allow_netduetto
+           is_nonarchive_timeshift_enabled is_timeshift_reserved).each do |key|
+          item = status.at(key)
+          result[key.sub(/^is_/,'').concat('?').to_sym] = item.inner_text == '1' if item
+        end
+
+        # Datetimes
+        %w(base_time open_time start_time end_time).each do |key|
+          item = status.at(key)
+          result[key.to_sym] = Time.at(item.inner_text.to_i) if item
+        end
+
+        rtmp = status.at('rtmp')
+        result[:rtmp] = {
+          url: rtmp.at('url').inner_text,
+          ticket: rtmp.at('ticket').inner_text,
+        }
+
+        ms = status.at('ms')
+        result[:ms] = {
+          address: ms.at('addr').inner_text,
+          port:    ms.at('port').inner_text.to_i,
+          thread:  ms.at('thread').inner_text,
+        }
+
+        quesheet = status.search('quesheet que')
+        result[:quesheet] = quesheet.map do |que|
+          {vpos: que['vpos'].to_i, mail: que['mail'], name: que['name'], body: que.inner_text}
+        end
+
+        result
+      end
+
+      def watching_reservations
+        page = agent.get(URL_WATCHINGRESERVATION_LIST)
+        page.search('vid').map(&:inner_text).map{ |_| normalize_id(_) }
+      end
+
+      def accept_watching_reservation(id_)
+        id = normalize_id(id_, with_lv: false)
+        page = agent.get("http://live.nicovideo.jp/api/watchingreservation?mode=confirm_watch_my&vid=#{id}&next_url&analytic")
+        token = page.at('#reserve img')['onclick'].scan(/'(.+?)'/)[1][0]
+
+        page = agent.post("http://live.nicovideo.jp/api/watchingreservation",
+                          accept: 'true', mode: 'use', vid: id, token: token)
+
+        page.at('nicolive_video_response')['status'] == 'ok'
+      end
+
+      private
+
+      def normalize_id(id, with_lv: true)
+        id = id.to_s
+
+        if with_lv
+          id.start_with?('lv') ? id : "lv#{id}"
+        else
+          id.start_with?('lv') ? id[2..-1] : id
+        end
+      end
+    end
+  end
+end

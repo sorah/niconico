sorah/niconico

commit 431d57c3832b6056ac993a02706fdf4374cb8cb9

Extract deferring logic into module

lib/niconico/deferrable.rb
@@ -1,0 +1,40 @@
+class Niconico
+  module Deferrable
+    module ClassMethods
+      def deferrable(*keys)
+        keys.each do |key|
+          binding.eval(<<-EOM, __FILE__, __LINE__.succ)
+            define_method(:#{key}) do
+              get() unless fetched?
+              @#{key}
+            end
+          EOM
+        end
+        self.deferred_methods.push *keys
+      end
+
+      def deferred_methods
+        @deferred_methods ||= []
+      end
+    end
+
+    def self.included(klass)
+      klass.extend ClassMethods
+    end
+
+    def fetched?; @fetched; end
+
+    def get
+      @fetched = true
+    end
+
+    private
+
+    def preload_deffered_values(vars={})
+      vars.each do |k,v|
+        next unless self.class.deferred_methods.include?(k)
+        instance_variable_set "@#{k}", v
+      end
+    end
+  end
+end

lib/niconico/video.rb
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 require 'json'
+require 'niconico/deferrable'
 
 class Niconico
   def video(video_id)
@@ -8,35 +9,29 @@ class Niconico
   end
 
   class Video
-    DEFERRABLES = [:id, :title, :url, :video_url, :type, :tags, :mylist_comment, :description, :description_raw]
-    DEFERRABLES_VAR = DEFERRABLES.map{|k| :"@#{k}" }
+    include Niconico::Deferrable
 
-    DEFERRABLES.zip(DEFERRABLES_VAR).each do |(k,i)|
-      define_method(k) do
-        instance_variable_get(i) || (get && instance_variable_get(i))
-      end
-    end
+    deferrable :id, :title,
+      :description, :description_raw,
+      :url, :video_url, :type,
+      :tags, :mylist_comment
 
     def initialize(parent, video_id, defer=nil)
       @parent = parent
       @agent = parent.agent
       @fetched = false
       @thread_id = @id = video_id
+      @page = nil
       @url = "#{Niconico::URL[:watch]}#{@id}"
 
       if defer
-        defer.each do |k,v|
-          next unless DEFERRABLES.include?(k)
-          instance_variable_set :"@#{k}", v
-        end
-        @page = nil
+        preload_deffered_values(defer)
       else
-        @page = get()
+        get()
       end
     end
 
     def economy?; @eco; end
-    def fetched?; @fetched; end
 
     def get(options = {})
       begin

commit 6895718c6a41373dc35776ba66c6c22cc05c8374

Don't request always even if it's preloaded value
This bug was introduced in 431d57c3832b6056ac993a02706fdf4374cb8cb9

lib/niconico/deferrable.rb
@@ -5,7 +5,7 @@ class Niconico
         keys.each do |key|
           binding.eval(<<-EOM, __FILE__, __LINE__.succ)
             define_method(:#{key}) do
-              get() unless fetched?
+              get() if @#{key}.nil? && !fetched?
               @#{key}
             end
           EOM
adfb351d03ba5a385caf7919d6eb2021087e5e64
yayugu <yayugu@gmail.com>
2015-05-27 01:21:17 +0900

    Niconico live search feature

--- lib/niconico/live/client.rb
+++ lib/niconico/live/client.rb
@@ -1,3 +1,4 @@
+require 'niconico/live/search_result'
 class Niconico
   def live_client
     Live::Client.new(self.agent)
@@ -5,6 +6,19 @@ class Niconico
 
   class Live
     class Client
+      SEARCH_FILTER_ONAIR = ':onair:' # 放送中
+      SEARCH_FILTER_RESERVED = ':reserved:' # 放送予定
+      SEARCH_FILTER_CLOSED = ':closed:' # 放送終了
+
+      # joined by 'OR'
+      SEARCH_FILTER_OFFICIAL = ':official:' # 公式
+      SEARCH_FILTER_CHANNEL = ':channel:' # チャンネル
+      SEARCH_FILTER_COMMUNITY = ':community:' # コミュニティ
+
+      SEARCH_FILTER_HIDE_TS_EXPIRED = ':hidetsexpired:' # タイムシフトが視聴できない番組を表示しない
+      SEARCH_FILTER_NO_COMMUNITY_GROUP = ':nocommunitygroup:' # 同一コミュニティをまとめて表示しない
+      SEARCH_FILTER_HIDE_COM_ONLY = ':hidecomonly:' # コミュニティ限定番組を表示しない
+
       def initialize(agent)
         @agent = agent
         @api = API.new(agent)
@@ -25,6 +39,30 @@ class Niconico
           post_body,
           'Content-Type' => 'application/x-www-form-urlencoded'
         )
+      end
+
+      def search(keyword, filters = [])
+        filter = filters.join('+')
+        page = @agent.get(
+          'http://live.nicovideo.jp/search',
+          track: '',
+          sort: 'recent',
+          date: '',
+          kind: '',
+          keyword: keyword,
+          filter: filter
+        )
+        results_dom = page.at('.result_list')
+        items = results_dom.css('.result_item')
+        search_results = items.map do |item|
+          title_dom = item.at('.search_stream_title a')
+          next nil unless title_dom
+          id = title_dom.attr(:href).scan(/lv[\d]+/).first
+          title = title_dom.text.strip
+          description = item.at('.search_stream_description').text.strip
+          SearchResult.new(id, title, description)
+        end
+        search_results.compact
       end
     end
   end
---
+++ lib/niconico/live/search_result.rb
@@ -0,0 +1,6 @@
+class Niconico
+  class Live
+    class SearchResult < Struct.new(:id, :title, :description)
+    end
+  end
+end
